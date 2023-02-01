b4c04acc1fdb05e2acbb8bdf4986f5c5df9d3dbc
Jane <evgenia.kochukova@gmail.com>
2023-02-01 16:31:42 -0300

    First

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 300;
+        int miles = 20;
+
+        System.out.println("Количество бонусных миль за покупку:");
+        System.out.println(( amount / miles ) + " миль(и) ");
+
+    }
+}
