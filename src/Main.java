16f97ba2df7981053706d28add18e7119900a997
Jane <evgenia.kochukova@gmail.com>
2023-02-03 21:46:33 -0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amount = 300;
-        int miles = 20;
-
-        System.out.println("Количество бонусных миль за покупку:");
-        System.out.println(( amount / miles ) + " миль(и) ");
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
